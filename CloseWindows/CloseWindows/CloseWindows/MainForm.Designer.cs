048c06a076f4eb12b5ca888c414e2ea10fa09d05
ZZH521 <936550332@qq.com>
2018-03-06 07:10:20 +0800

    2
    增加关机功能

--- CloseWindows/CloseWindows/CloseWindows/MainForm.Designer.cs
+++ CloseWindows/CloseWindows/CloseWindows/MainForm.Designer.cs
@@ -28,16 +28,25 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.button1 = new System.Windows.Forms.Button();
             this.tbCommand = new System.Windows.Forms.TextBox();
             this.tbCommandResult = new System.Windows.Forms.TextBox();
+            this.lbTimer = new System.Windows.Forms.Label();
+            this.timerComputer = new System.Windows.Forms.Timer(this.components);
+            this.btCloseWin = new System.Windows.Forms.Button();
+            this.btCancel = new System.Windows.Forms.Button();
+            this.dtTimer = new System.Windows.Forms.DateTimePicker();
+            this.lbDifValue = new System.Windows.Forms.Label();
+            this.timerCntDown = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(350, 28);
+            this.button1.Location = new System.Drawing.Point(467, 35);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(62, 23);
+            this.button1.Size = new System.Drawing.Size(83, 29);
             this.button1.TabIndex = 0;
             this.button1.Text = "Run";
             this.button1.UseVisualStyleBackColor = true;
@@ -45,29 +54,96 @@
             // 
             // tbCommand
             // 
-            this.tbCommand.Location = new System.Drawing.Point(42, 28);
+            this.tbCommand.Location = new System.Drawing.Point(56, 35);
+            this.tbCommand.Margin = new System.Windows.Forms.Padding(4);
             this.tbCommand.Name = "tbCommand";
-            this.tbCommand.Size = new System.Drawing.Size(269, 21);
+            this.tbCommand.Size = new System.Drawing.Size(357, 25);
             this.tbCommand.TabIndex = 1;
             // 
             // tbCommandResult
             // 
             this.tbCommandResult.BackColor = System.Drawing.SystemColors.InfoText;
             this.tbCommandResult.ForeColor = System.Drawing.SystemColors.Window;
-            this.tbCommandResult.Location = new System.Drawing.Point(42, 87);
+            this.tbCommandResult.Location = new System.Drawing.Point(56, 165);
+            this.tbCommandResult.Margin = new System.Windows.Forms.Padding(4);
             this.tbCommandResult.Multiline = true;
             this.tbCommandResult.Name = "tbCommandResult";
-            this.tbCommandResult.Size = new System.Drawing.Size(370, 188);
+            this.tbCommandResult.Size = new System.Drawing.Size(492, 234);
             this.tbCommandResult.TabIndex = 2;
+            // 
+            // lbTimer
+            // 
+            this.lbTimer.AutoSize = true;
+            this.lbTimer.ForeColor = System.Drawing.Color.Blue;
+            this.lbTimer.Location = new System.Drawing.Point(56, 72);
+            this.lbTimer.Name = "lbTimer";
+            this.lbTimer.Size = new System.Drawing.Size(159, 15);
+            this.lbTimer.TabIndex = 3;
+            this.lbTimer.Text = "0000-00-00 00:00:00";
+            // 
+            // timerComputer
+            // 
+            this.timerComputer.Interval = 1000;
+            this.timerComputer.Tick += new System.EventHandler(this.timerComputer_Tick);
+            // 
+            // btCloseWin
+            // 
+            this.btCloseWin.Location = new System.Drawing.Point(330, 75);
+            this.btCloseWin.Name = "btCloseWin";
+            this.btCloseWin.Size = new System.Drawing.Size(83, 29);
+            this.btCloseWin.TabIndex = 4;
+            this.btCloseWin.Text = "Close";
+            this.btCloseWin.UseVisualStyleBackColor = true;
+            this.btCloseWin.Click += new System.EventHandler(this.btCloseWin_Click);
+            // 
+            // btCancel
+            // 
+            this.btCancel.Location = new System.Drawing.Point(467, 75);
+            this.btCancel.Name = "btCancel";
+            this.btCancel.Size = new System.Drawing.Size(83, 29);
+            this.btCancel.TabIndex = 5;
+            this.btCancel.Text = "Cancel";
+            this.btCancel.UseVisualStyleBackColor = true;
+            this.btCancel.Click += new System.EventHandler(this.btCancel_Click);
+            // 
+            // dtTimer
+            // 
+            this.dtTimer.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.dtTimer.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtTimer.Location = new System.Drawing.Point(56, 93);
+            this.dtTimer.Name = "dtTimer";
+            this.dtTimer.Size = new System.Drawing.Size(197, 25);
+            this.dtTimer.TabIndex = 6;
+            // 
+            // lbDifValue
+            // 
+            this.lbDifValue.AutoSize = true;
+            this.lbDifValue.ForeColor = System.Drawing.Color.Green;
+            this.lbDifValue.Location = new System.Drawing.Point(56, 130);
+            this.lbDifValue.Name = "lbDifValue";
+            this.lbDifValue.Size = new System.Drawing.Size(67, 15);
+            this.lbDifValue.TabIndex = 7;
+            this.lbDifValue.Text = "取消关机";
+            // 
+            // timerCntDown
+            // 
+            this.timerCntDown.Interval = 1000;
+            this.timerCntDown.Tick += new System.EventHandler(this.timerCntDown_Tick);
             // 
             // Mainform
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(456, 287);
+            this.ClientSize = new System.Drawing.Size(608, 430);
+            this.Controls.Add(this.lbDifValue);
+            this.Controls.Add(this.dtTimer);
+            this.Controls.Add(this.btCancel);
+            this.Controls.Add(this.btCloseWin);
+            this.Controls.Add(this.lbTimer);
             this.Controls.Add(this.tbCommandResult);
             this.Controls.Add(this.tbCommand);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Mainform";
             this.Text = "调用cmd.exe并显示指令结果";
             this.Load += new System.EventHandler(this.Mainform_Load);
@@ -81,6 +157,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox tbCommand;
         private System.Windows.Forms.TextBox tbCommandResult;
+        private System.Windows.Forms.Label lbTimer;
+        private System.Windows.Forms.Timer timerComputer;
+        private System.Windows.Forms.Button btCloseWin;
+        private System.Windows.Forms.Button btCancel;
+        private System.Windows.Forms.DateTimePicker dtTimer;
+        private System.Windows.Forms.Label lbDifValue;
+        private System.Windows.Forms.Timer timerCntDown;
     }
 }
 
--- CloseWindows/CloseWindows/CloseWindows/MainForm.cs
+++ CloseWindows/CloseWindows/CloseWindows/MainForm.cs
@@ -20,7 +20,8 @@ namespace CloseWindows
 
         private void Mainform_Load(object sender, EventArgs e)
         {
-
+            Timer timer = new Timer();
+            timerComputer.Start();
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -92,6 +93,139 @@ namespace CloseWindows
             }
             CmdProcess.WaitForExit();//等待程序执行完退出进程
             CmdProcess.Close();     
+        }
+
+        private void timerComputer_Tick(object sender, EventArgs e)
+        {
+            lbTimer.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        private void btCancel_Click(object sender, EventArgs e)
+        {
+            string cmd = "shutdown -a";//取消关机
+            RunProgram(cmd);
+            //lbDifValue.Text = "0" + "天" + "0" + "小时" + "0" + "分" + "秒";
+            lbDifValue.Text = "取消关闭计算机";
+            lbDifValue.ForeColor = System.Drawing.Color.Green;
+            timerCntDown.Stop();
+        }
+        public int num = 0;
+        public int cutNum = 0;
+        public int daySecond = 0;
+        public int hourSecond = 0;
+        public int minuteSecond = 0;
+        public int second = 0;
+        public int sumSecond = 0;
+        private void btCloseWin_Click(object sender, EventArgs e)
+        {           
+            char[] chars =  DateDiff(dtTimer.Value).ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+
+                if (chars[i] == Convert.ToChar("天"))
+                {
+                    num = i - 0;//天的位数
+                    cutNum = i;//天之前的数据占得长度
+                    if (chars[i-1] != 48)//不为0
+                    {
+                        for (int j = 0; j < i; j++)
+                        {
+                            num--;
+                            daySecond += (chars[j]-48) * 24 * 60 * 60 * Convert.ToInt16( Math.Pow(10, num));                            
+                        }
+                    }
+                    cutNum = cutNum + 1; //天之前的数据占得长度 +包括天字本身
+                }
+                else if (chars[i] == Convert.ToChar("小") && chars[i+1] == Convert.ToChar("时"))
+                {
+                    num = i - cutNum;//小时的位数
+                    cutNum = i; //天跟小时之间的数据长度
+                    if (chars[i - 1] != 48)//不为0
+                    {
+                        for (int j = cutNum - num; j < i; j++)
+                        {
+                            num--;
+                            hourSecond += (chars[j] - 48) * 60 * 60 * Convert.ToInt16(Math.Pow(10, num));
+                        }
+                    }
+                    cutNum = cutNum + 2; //天跟小时之间的数据长度 + 包括小时本身
+                }
+                else if (chars[i] == Convert.ToChar("分") && chars[i + 1] == Convert.ToChar("钟"))
+                {
+                    num = i - cutNum;//分钟的位数
+                    cutNum = i; //小时跟分钟之间的数据长度
+                    if (chars[i - 1] != 48)//不为0
+                    {
+                        for (int j = cutNum- num; j < i; j++)
+                        {
+                            num--;
+                            minuteSecond += (chars[j] - 48) * 60 * Convert.ToInt16(Math.Pow(10, num));
+                        }
+                    }
+                    cutNum = cutNum + 2; //小时跟分钟之间的数据长度 + 包括分钟本身
+                }
+                else if (chars[i] == Convert.ToChar("秒") )
+                {
+                    num = i - cutNum;//秒的位数
+                    cutNum = i; //小时跟分钟之间的数据长度
+                    for (int j = cutNum- num; j < i; j++)
+                    {
+                        num--;
+                        minuteSecond += (chars[j] -48) * Convert.ToInt16(Math.Pow(10, num));
+                    }
+                }
+            }
+            sumSecond = daySecond + hourSecond + minuteSecond + second;//总时间
+            string stdown;
+            stdown = "shutdown"+ " -"+"s"+ " -"+"t "+ Convert.ToString(sumSecond) ;
+            RunProgram(stdown);
+            lbDifValue.Text = DateDiff(dtTimer.Value);
+            lbDifValue.ForeColor = System.Drawing.Color.Red;
+            daySecond = 0;
+            hourSecond = 0;
+            minuteSecond = 0;
+            second = 0;
+            sumSecond = 0;
+            timerCntDown.Start();
+        }
+        /// <summary>
+        /// 已重载.计算一个时间与当前本地日期和时间的时间间隔,返回的是时间间隔的日期差的绝对值.
+        /// </summary>
+        /// <param name="DateTime1">一个日期和时间</param>
+        /// <returns></returns>
+        private string DateDiff(DateTime DateTime1)
+        {
+            return this.DateDiff(DateTime1, DateTime.Now);
+        }
+        /// <summary>
+        /// 已重载.计算两个日期的时间间隔,返回的是时间间隔的日期差的绝对值.
+        /// </summary>
+        /// <param name="DateTime1">第一个日期和时间</param>
+        /// <param name="DateTime2">第二个日期和时间</param>
+        /// <returns></returns>
+        private string DateDiff(DateTime DateTime1, DateTime DateTime2)
+        {
+            string dateDiff = null;
+            try
+            {
+                TimeSpan ts1 = new TimeSpan(DateTime1.Ticks);
+                TimeSpan ts2 = new TimeSpan(DateTime2.Ticks);
+                TimeSpan ts = ts1.Subtract(ts2).Duration();
+                dateDiff = ts.Days.ToString() + "天"
+                        + ts.Hours.ToString() + "小时"
+                        + ts.Minutes.ToString() + "分钟"
+                        + ts.Seconds.ToString() + "秒";
+            }
+            catch
+            {
+
+            }
+            return dateDiff;
+        }
+
+        private void timerCntDown_Tick(object sender, EventArgs e)
+        {
+
         }
     }
 }
